41841bc3188a2040356b5b424d4b53fa3bf893a2
karan@guardrailsai.com <thekaranacharya@gmail.com>
2024-02-22 16:25:58 -0500

    Add makefile and tests

---
+++ test/test_validator.py
@@ -0,0 +1,54 @@
+from guardrails import Guard
+from pydantic import BaseModel, Field
+from validator import ValidLength
+import pytest
+
+
+# Create a pydantic model with a field that uses the custom validator
+class ValidatorTestObject(BaseModel):
+    text: str = Field(validators=[ValidLength(min=3, max=6, on_fail="exception")])
+
+
+# Test happy path
+@pytest.mark.parametrize(
+    "value",
+    [
+        """
+        {
+            "text": "hello"
+        }
+        """
+    ],
+)
+def test_happy_path(value):
+    """Test the happy path for the validator."""
+    # Create a guard from the pydantic model
+    guard = Guard.from_pydantic(output_class=ValidatorTestObject)
+    response = guard.parse(value)
+    print("Happy path response", response)
+    assert response.validation_passed is True
+
+
+# Test fail path
+@pytest.mark.parametrize(
+    "value",
+    [
+        """
+        {
+            "text": "hi"
+        }
+        """,
+        """
+        {
+            "text": "educational"
+        }
+        """,
+    ],
+)
+def test_fail_path(value):
+    # Create a guard from the pydantic model
+    guard = Guard.from_pydantic(output_class=ValidatorTestObject)
+
+    with pytest.raises(Exception):
+        response = guard.parse(value)
+        print("Fail path response", response)
